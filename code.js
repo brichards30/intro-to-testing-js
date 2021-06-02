629a75b807af8bd07becac3c4908de07547595a8
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-02 12:22:56 -0500

    add tests and ability to say 'unit tests for edge cases.

--- code.js
+++ code.js
@@ -4,14 +4,17 @@ function helloWorld() {
 }
 
 function sayHello(name) {
-    return 'Hello, ' + name + '!';
-    /*if (name === "Jaymes") {
+    // return 'Hello, ' + name + '!';
+    if (name === "Jaymes") {
         return "Hello, Jaymes!"
     } else if (name === "Alex") {
         return 'Hello, Alex!';
     } else if (name === "Pat") {
         return 'Hello, Pat!';
-    }
-    return 'Hello, World!'
-     */
+    } else if (name === true) {
+        return 'Hello, World!';
+    } //else if (name === false) {
+    return 'Hello, World!';
+
+
 }
